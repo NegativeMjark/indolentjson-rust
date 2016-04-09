09de974f40cd3151a8bc2da560901f5d7432e46c
Mark Haines <mjark@negativecurvature.net>
2016-04-09 13:39:27 +0100

    Split out compacting unicode into a separate fn

--- src/compact.rs
+++ src/compact.rs
@@ -49,89 +49,9 @@ fn compact_vector_(input: &[u8], output: &mut Vec<u8>) -> bool {
                         Some(value) => *value,
                     };
                     if input_char == b'u' { // Unicode escape "u"
-                        let h0 = match iter.next() {
-                            None => return false,
-                            Some(value) => *value
-                        };
-                        let h1 = match iter.next() {
-                            None => return false,
-                            Some(value) => *value
-                        };
-                        let h2 = match iter.next() {
-                            None => return false,
-                            Some(value) => *value
-                        };
-                        let h3 = match iter.next() {
-                            None => return false,
-                            Some(value) => *value
-                        };
-                        let escaped = read_hexdigits(h0, h1, h2, h3);
-                        if escaped < 0x20 {
-                            output.push(b'\\');
-                            match escaped {
-                                0x08 => output.push(b'b'),
-                                0x09 => output.push(b't'),
-                                0x0A => output.push(b'n'),
-                                0x0C => output.push(b'f'),
-                                0x0D => output.push(b'r'),
-                                _ => {
-                                    output.push(b'u');
-                                    output.push(b'0');
-                                    output.push(b'0');
-                                    output.push(b'0' + (escaped as u8 >> 4));
-                                    output.push(HEX[(escaped & 0xF) as usize]);
-                                },
-                            }
-                        } else if escaped < 0x80 {
-                            if escaped as u8 == b'\"' || escaped as u8 == b'\\' {
-                                output.push(b'\\');
-                            }
-                            output.push(escaped as u8);
-                        } else if escaped < 0x800 {
-                            output.push((escaped >> 6) as u8 | 0xC0);
-                            output.push((escaped as u8 & 0x3F) | 0x80);
-                        } else {
-                            if escaped < 0xD800 || escaped >= 0xE000 {
-                                output.push((escaped >> 12) as u8 | 0xE0);
-                                output.push(((escaped >> 6) & 0x3F) as u8 | 0x80);
-                                output.push((escaped as u8 & 0x3F) | 0x80);
-                            } else {
-                                // surrogate pair
-                                match iter.next() {
-                                    None => return false,
-                                    _ => {;}
-                                }
-                                match iter.next() {
-                                    None => return false,
-                                    _ => {;}
-                                }
-                                let h0 = match iter.next() {
-                                    None => return false,
-                                    Some(value) => *value
-                                };
-                                let h1 = match iter.next() {
-                                    None => return false,
-                                    Some(value) => *value
-                                };
-                                let h2 = match iter.next() {
-                                    None => return false,
-                                    Some(value) => *value
-                                };
-                                let h3 = match iter.next() {
-                                    None => return false,
-                                    Some(value) => *value
-                                };
-                                let surrogate = read_hexdigits(h0, h1, h2, h3);
-                                let codepoint = 0x10000 + (
-                                    ((escaped & 0x3FF) << 10) | (surrogate & 0x3FF)
-                                );
-                                output.push((codepoint >> 18) as u8 | 0xF0);
-                                output.push(((codepoint >> 12) & 0x3F) as u8 | 0x80);
-                                output.push(((codepoint >> 6) & 0x3F) as u8 | 0x80);
-                                output.push((codepoint as u8 & 0x3F) | 0x80);
-                            }
+                        if !compact_unicode_escape(&mut iter, output) {
+                            return false
                         }
-
                     } else if input_char == b'/' { // Forward Slash '/'
                         output.push(input_char);
                     } else {
@@ -147,6 +67,88 @@ fn compact_vector_(input: &[u8], output: &mut Vec<u8>) -> bool {
             }
         }
     }
+}
+
+fn compact_unicode_escape<'a, T: Iterator<Item=&'a u8>>(iter: &mut T, output: &mut Vec<u8>) -> bool {
+    let h0 = match iter.next() {
+        None => return false,
+        Some(value) => *value
+    };
+    let h1 = match iter.next() {
+        None => return false,
+        Some(value) => *value
+    };
+    let h2 = match iter.next() {
+        None => return false,
+        Some(value) => *value
+    };
+    let h3 = match iter.next() {
+        None => return false,
+        Some(value) => *value
+    };
+    let escaped = read_hexdigits(h0, h1, h2, h3);
+    if escaped < 0x20 {
+        output.push(b'\\');
+        match escaped {
+            0x08 => output.push(b'b'),
+            0x09 => output.push(b't'),
+            0x0A => output.push(b'n'),
+            0x0C => output.push(b'f'),
+            0x0D => output.push(b'r'),
+            _ => {
+                output.push(b'u');
+                output.push(b'0');
+                output.push(b'0');
+                output.push(b'0' + (escaped as u8 >> 4));
+                output.push(HEX[(escaped & 0xF) as usize]);
+            },
+        }
+    } else if escaped < 0x80 {
+        if escaped as u8 == b'\"' || escaped as u8 == b'\\' {
+            output.push(b'\\');
+        }
+        output.push(escaped as u8);
+    } else if escaped < 0x800 {
+        output.push((escaped >> 6) as u8 | 0xC0);
+        output.push((escaped as u8 & 0x3F) | 0x80);
+    } else if escaped < 0xD800 || escaped >= 0xE000 {
+        output.push((escaped >> 12) as u8 | 0xE0);
+        output.push(((escaped >> 6) & 0x3F) as u8 | 0x80);
+        output.push((escaped as u8 & 0x3F) | 0x80);
+    } else {
+        // surrogate pair
+        if iter.next() == None {
+            return false
+        }
+        if iter.next() == None {
+            return false
+        }
+        let h0 = match iter.next() {
+            None => return false,
+            Some(value) => *value
+        };
+        let h1 = match iter.next() {
+            None => return false,
+            Some(value) => *value
+        };
+        let h2 = match iter.next() {
+            None => return false,
+            Some(value) => *value
+        };
+        let h3 = match iter.next() {
+            None => return false,
+            Some(value) => *value
+        };
+        let surrogate = read_hexdigits(h0, h1, h2, h3);
+        let codepoint = 0x10000 + (
+            ((escaped & 0x3FF) << 10) | (surrogate & 0x3FF)
+        );
+        output.push((codepoint >> 18) as u8 | 0xF0);
+        output.push(((codepoint >> 12) & 0x3F) as u8 | 0x80);
+        output.push(((codepoint >> 6) & 0x3F) as u8 | 0x80);
+        output.push((codepoint as u8 & 0x3F) | 0x80);
+    }
+    return true;
 }
 
 pub fn compact(input_json: &str) -> String {
